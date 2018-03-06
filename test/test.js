49987aec6bde0d5f494fec17b770ecf552cd3430
BJR Matos <bjrmatos@gmail.com>
2018-03-06 12:29:47 -0500

    add initial implementation

--- test/test.js
+++ test/test.js
@@ -1,5 +1,103 @@
 'use strict'
 
-describe('phantom-page-eval', () => {
+const should = require('should')
+const path = require('path')
+const phantomPath = require('phantomjs').path
+const phantomPageEval = require('../')
 
+const sampleHtmlPath = path.join(__dirname, 'sample.html')
+
+describe('phantom-page-eval', () => {
+  let phantomEval
+
+  beforeEach(() => {
+    phantomEval = phantomPageEval({ phantomPath })
+  })
+
+  it('should fail when no phantomPath is specified', () => {
+    should(() => {
+      phantomPageEval()
+    }).throw()
+  })
+
+  it('should fail when required options are not specified', async () => {
+    return should(
+      phantomEval()
+    ).be.rejected()
+  })
+
+  it('should fail with invalid script', async () => {
+    return should(
+      phantomEval({
+        html: sampleHtmlPath,
+        scriptFn: `1 + 2`
+      })
+    ).be.rejected()
+  })
+
+  it('should eval simple script', async () => {
+    const result = await phantomEval({
+      html: sampleHtmlPath,
+      scriptFn: 'function () { return 1 + 2 }'
+    })
+
+    should(result).be.eql(3)
+  })
+
+  it('should eval script that uses DOM', async () => {
+    const result = await phantomEval({
+      html: sampleHtmlPath,
+      scriptFn: `
+        function () {
+          var title = document.title
+          var content = Array.prototype.slice.call(document.getElementsByClassName('content'))
+
+          content = content.map(function (node) {
+            return node.textContent
+          })
+
+          return {
+            title: title,
+            content: content
+          }
+        }
+      `
+    })
+
+    should(result).be.Object()
+    should(result.title).be.eql('Test page')
+    should(result.content).have.length(4)
+  })
+
+  it('should pass custom args to script', async () => {
+    const result = await phantomEval({
+      html: sampleHtmlPath,
+      args: [1, 2],
+      scriptFn: `
+        function (x, y) {
+          return x + y
+        }
+      `
+    })
+
+    should(result).be.eql(3)
+  })
+
+  it('should timeout with blocking script', async () => {
+    return should(
+      phantomEval({
+        html: sampleHtmlPath,
+        timeout: 1500,
+        scriptFn: `
+          function () {
+            while (true) {
+
+            }
+
+            return 'test'
+          }
+        `
+      })
+    ).be.rejectedWith(/Timeout/)
+  })
 })
